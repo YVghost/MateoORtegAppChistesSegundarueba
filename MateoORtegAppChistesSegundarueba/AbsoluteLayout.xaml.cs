14c28bb8b5eb4f7b4bd781c0944dda1d258ec307
mate <dddmateo@gmail.com>
2025-06-03 08:25:27 -0500

    api consumir

--- MateoORtegAppChistesSegundarueba/AbsoluteLayout.xaml.cs
+++ MateoORtegAppChistesSegundarueba/AbsoluteLayout.xaml.cs
@@ -1,3 +1,5 @@
+using System.Net.Http.Json;
+
 namespace MateoORtegAppChistesSegundarueba;
 
 public partial class AbsoluteLayout : ContentPage
@@ -6,4 +8,31 @@ public partial class AbsoluteLayout : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void OnOtroChisteClicked(object sender, EventArgs e)
+    {
+        try
+        {
+            using var client = new HttpClient();
+            var joke = await client.GetFromJsonAsync<Joke>("https://official-joke-api.appspot.com/random_joke");
+            if (joke != null)
+            {
+                JokeLabel.Text = $"{joke.setup}\n{joke.punchline}";
+            }
+            else
+            {
+                JokeLabel.Text = "No se pudo obtener un chiste.";
+            }
+        }
+        catch
+        {
+            JokeLabel.Text = "Error al obtener el chiste.";
+        }
+    }
+
+    public class Joke
+    {
+        public string setup { get; set; }
+        public string punchline { get; set; }
+    }
 }
