336138e3967931131a56b8683e5d2f1208168342
mate <dddmateo@gmail.com>
2025-06-03 07:51:39 -0500

    Agregar archivos de proyecto.

---
+++ MateoORtegAppChistesSegundarueba/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MateoORtegAppChistesSegundarueba
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
